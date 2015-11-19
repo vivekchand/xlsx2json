83a33dcb1e319fd44f6b388578a3818684513f75
Vivek Chand <vivek@recruiterbox.com>
2015-11-19 09:09:41 +0530

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,19 @@
+from openpyxl import load_workbook
+
+
+def parse_mix_report(path):
+    wb = load_workbook(path)
+    ws = wb.worksheets[0]
+    rows = ws.rows
+    header_row = rows[0]
+    content_rows = rows[1:]
+    data_dict = []
+
+    for row in content_rows:
+        pos = 0
+        row_dict = {}
+        for header in header_row:
+            row_dict[header.value] = row[pos].value
+            pos += 1
+        data_dict.append(row_dict)
+    return data_dict
